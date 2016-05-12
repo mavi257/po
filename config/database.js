403a294457f60d08e62a1a2cb8f27508e302c58b
vivek <viveksignedin@gmail.com>
2016-05-12 18:22:44 +0530

    make it bettesr

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'your-database-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://v:as@ds021182.mlab.com:21182/zz' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
