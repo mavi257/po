7ab9fa0c0d70a482fe432bb07031e24076ba2f07
vivek <viveksignedin@gmail.com>
2016-05-12 16:17:48 +0530

    make it better

--- config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-
-	'url' : 'mongodb://vi:123@ds019482.mlab.com:19482/as' 
-
-};
--- server.js
+++ server.js
@@ -1,41 +1,25 @@
-// server.js
+var restify     =   require('restify');
+var mongojs     =   require('mongojs');
+var	morgan  	= 	require('morgan');
+var db          =   mongojs('mongodb://vi:123@ds019482.mlab.com:19482/as', ['users','bucketLists']);
+var server      =   restify.createServer();
 
-// set up ======================================================================
-// get all the tools we need
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 8080;
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash    = require('connect-flash');
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+server.use(morgan('dev')); // LOGGER
 
-var configDB = require('./config/database.js');
-
-// configuration ===============================================================
-mongoose.connect(configDB.url); // connect to our database
-
-require('./config/passport')(passport); // pass passport for configuration
-
-app.configure(function() {
-
-	// set up our express application
-	app.use(express.logger('dev')); // log every request to the console
-	app.use(express.cookieParser()); // read cookies (needed for auth)
-	app.use(express.bodyParser()); // get information from html forms
-
-	app.set('view engine', 'ejs'); // set up ejs for templating
-
-	// required for passport
-	app.use(express.session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
-	app.use(passport.initialize());
-	app.use(passport.session()); // persistent login sessions
-	app.use(flash()); // use connect-flash for flash messages stored in session
-
+// CORS
+server.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
 });
 
-// routes ======================================================================
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+server.listen(process.env.PORT || 9804, function () {
+    console.log("Server started @ ", process.env.PORT || 9804);
+});
 
-// launch ======================================================================
-app.listen(port);
-console.log('The magic happens on port ' + port);
+var manageUsers =   require('./auth/manageUser')(server, db);
+var manageLists =   require('./list/manageList')(server, db);
